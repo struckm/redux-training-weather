8a334da758ce0c3ed4ebcfd84080da25b0ee3cc4
Mark Struck <markstruck@comcast.net>
2016-08-21 16:10:49 -0500

    Updated package versions and added chart

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
     constructor(props, context) {
         super(props);
+    }
+
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+                <td>
+                    <Chart data={temps} color="orange" />
+                </td>
+            </tr>
+        );
     }
 
     render() {
@@ -18,7 +33,7 @@ class WeatherList extends Component {
                     </tr>
                 </thead>
                 <tbody>
-
+                    {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
         );
